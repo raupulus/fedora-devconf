aa778302348c5a86b6ece95c4d5a111490a69c50
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-08 00:50:34 +0100

    Añadida cabecera

--- funciones.sh
+++ funciones.sh
@@ -1,9 +1,26 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+#######################################
+# ###     Raúl Caro Pastorino     ### #
+## ##                             ## ##
+### # https://github.com/fryntiz/ # ###
+## ##                             ## ##
+# ###       www.fryntiz.es        ### #
+#######################################
+
+############################
+##       Constantes      ##
+############################
+DIR_ACTUAL=`echo $PWD`
+
+
 # Crea un respaldo del archivo o directorio pasado como parámetro
 function crearBackup() {
     for salvando in $*
     do
         if [ -f $salvando ]; then
             echo "Creando Backup del archivo $salvando"
+
         elif [ -d $salvando ]; then
             echo "Creando Backup del directorio $salvando"
         else
