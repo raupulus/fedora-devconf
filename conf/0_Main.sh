b89c9dff806c5925943ee8230bb2a1ff7ea2d597
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-08 01:33:14 +0100

    Llamada a configuración de usuario

--- conf/0_Main.sh
+++ conf/0_Main.sh
@@ -1,6 +1,9 @@
-source 'git.sh'
 source '1_Configuraciones_Basicas.sh'
 source '2_Configuraciones_Extras.sh'
 source '3_Configuraciones_Usuario.sh'
+source 'git.sh'
 # Llama a la configuración de GIT
-configuracion_git
+#configuracion_git
+
+# Llama a configurar usuario
+configurar_Usuario
