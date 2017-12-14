26bc211e4c736ddead18ee23d8cd8d0555579ed7
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-14 01:04:04 +0100

    Añadidos directorios

--- apps/0_Main.sh
+++ apps/0_Main.sh
@@ -2,6 +2,11 @@ source "$DIR_ACTUAL/apps/1_Aplicaciones_Basicas.sh"
 source "$DIR_ACTUAL/apps/2_Aplicaciones_Extras.sh"
 source "$DIR_ACTUAL/apps/3_Aplicaciones_Usuario.sh"
 source "$DIR_ACTUAL/apps/bashit.sh"
+source "$DIR_ACTUAL/apps/i3wm.sh"
+source "$DIR_ACTUAL/apps/vim.sh"
 
 # Llama al instalador de Bash-it
 bashit_Instalador
+
+# Llama al instalador del gestor de ventanas i3wm
+i3wm_instalación
