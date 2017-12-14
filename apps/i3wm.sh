a401f50d89ae8137530be09e3df52cae85c24b5c
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-14 01:04:23 +0100

    Añadidas dependencias

--- apps/i3wm.sh
+++ apps/i3wm.sh
@@ -1,10 +1,11 @@
 function i3wm_dependencias() {
     echo "Instalando Dependencias"
-    $dependencias="i3 i3status dmenu i3lock xbacklight feh"
+    dependencias="i3 i3status dmenu i3lock xbacklight feh alsamixer nmcli mc links"
+
     for x in $dependencias
     do
         echo "Instalando $x"
-        sudo dnf install $x
+        sudo dnf install -y $x
     done
 }
 
