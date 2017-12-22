3cde124355dd49c76c378825718d87fba807767f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-22 02:29:49 +0100

    Siguiendo guía de estilos

--- conf/0_Main.sh
+++ conf/0_Main.sh
@@ -1,7 +1,44 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2017 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        www.fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
+
+############################
+##     INSTRUCCIONES      ##
+############################
+
+############################
+##     IMPORTACIONES      ##
+############################
 source "$DIR_ACTUAL/conf/1_Configuraciones_Basicas.sh"
 source "$DIR_ACTUAL/conf/2_Configuraciones_Extras.sh"
 source "$DIR_ACTUAL/conf/3_Configuraciones_Usuario.sh"
 source "$DIR_ACTUAL/conf/git.sh"
+
+############################
+##       CONSTANTES       ##
+############################
+
+###########################
+##       VARIABLES       ##
+###########################
+
+###########################
+##       FUNCIONES       ##
+###########################
+
+###########################
+##       EJECUCIÓN       ##
+###########################
 # Llama a la configuración de GIT
 #configuracion_git
 
