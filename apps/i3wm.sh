c2c7fb9e40b981657a86d2dbf64b7b8f04f0d717
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-22 19:09:28 +0100

    Siguiendo guía de estilos

--- apps/i3wm.sh
+++ apps/i3wm.sh
@@ -1,3 +1,36 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2017 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        www.fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
+
+############################
+##     INSTRUCCIONES      ##
+############################
+
+############################
+##     IMPORTACIONES      ##
+############################
+
+############################
+##       CONSTANTES       ##
+############################
+
+###########################
+##       VARIABLES       ##
+###########################
+
+###########################
+##       FUNCIONES       ##
+###########################
 i3wm_dependencias() {
     echo "Instalando Dependencias"
     dependencias="i3 i3status dmenu i3lock xbacklight feh alsamixer nmcli mc links"
@@ -8,6 +41,11 @@ i3wm_dependencias() {
         sudo dnf install -y $x
     done
 }
+
+###########################
+##       EJECUCIÓN       ##
+###########################
+
 
 i3wm_instalación() {
     i3wm_dependencias
