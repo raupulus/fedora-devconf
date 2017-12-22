0c4a0b97c36c4c11b33d93b4465dc13cc3ca5ad5
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-22 16:46:11 +0100

    Añadida plantilla de estilos

--- conf/3_Configuraciones_Usuario.sh
+++ conf/3_Configuraciones_Usuario.sh
@@ -1,13 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # -*- ENCODING: UTF-8 -*-
-#######################################
-# ###     Raúl Caro Pastorino     ### #
-## ##                             ## ##
-### # https://github.com/fryntiz/ # ###
-## ##                             ## ##
-# ###       www.fryntiz.es        ### #
-#######################################
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2017 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        www.fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
 
+############################
+##     INSTRUCCIONES      ##
+############################
+
+############################
+##     IMPORTACIONES      ##
+############################
+
+############################
+##       CONSTANTES       ##
+############################
+
+###########################
+##       VARIABLES       ##
+###########################
+
+###########################
+##       FUNCIONES       ##
+###########################
 function home() {
     # Mover al directorio Backups el archivo .bashrc
     if [ -f "$HOME/.bashrc" ]; then
@@ -21,3 +44,7 @@ function home() {
 function configurar_Usuario() {
     home
 }
+
+###########################
+##       EJECUCIÓN       ##
+###########################
