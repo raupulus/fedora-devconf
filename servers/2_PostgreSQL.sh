e48ec0124df2d6998b04396b3c7b1453d5d5e1aa
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-28 04:12:59 +0100

    Planteado comportamiento desde el script de debian

--- servers/2_PostgreSQL.sh
+++ servers/2_PostgreSQL.sh
@@ -32,15 +32,33 @@
 ##       FUNCIONES       ##
 ###########################
 postgresql_preconfiguracion() {
-
+    echo -e "$VE Generando Pre-Configuraciones de$RO PostgreSQL$CL"
 }
 
 postgresql_instalar() {
-
+    echo -e "$VE Instalando$RO PostgreSQL$CL"
+    sudo apt install -y postgresql postgresql-client >> /dev/null 2>> /dev/null
+    sudo apt install -y postgresql-contrib >> /dev/null 2>> /dev/null
+    sudo apt install -y postgresql-all >> /dev/null 2>> /dev/null
 }
 
 postgresql_postconfiguracion() {
+    echo -e "$VE Generando Post-Configuraciones$RO PostgreSQL$CL"
 
+    POSTGRESCONF="/etc/postgresql/9.6/main/postgresql.conf"  # Archivo de configuración para postgresql
+
+    echo -e "$verde Estableciendo intervalstyle = 'iso_8601'$gris"
+    sudo sed -r -i "s/^\s*#?intervalstyle\s*=/intervalstyle = 'iso_8601' #/" $POSTGRESCONF
+
+    echo -e "$verde Estableciendo timezone = 'UTC'$gris"
+    sudo sed -r -i "s/^\s*#?timezone\s*=/timezone = 'UTC' #/" $POSTGRESCONF
+
+    echo -e "$verde Personalizando SQL$gris"
+    #sudo -u postgres createdb basedatos #Crea la base de datos basedatos
+    #sudo -u postgres createuser -P usuario #Crea el usuario usuario y pide que teclee su contraseña
+
+    ## Reiniciar servidor postgresql al terminar
+    sudo systemctl restart postgresql
 }
 
 ###########################
